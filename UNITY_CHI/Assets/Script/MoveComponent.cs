fe71651173d90d1c4d123cf1203a0dc7e0acdf5a
hanil12 <ekdrhaod@gmail.com>
2022-06-16 16:35:01 +0900

    SolarSystem
    과제 : 태양계 구현
    1. 태양 자전
    2. 지구 : 자전 공전
    3. 달 : 자전 공전

--- UNITY_CHI/Assets/Script/MoveComponent.cs
+++ UNITY_CHI/Assets/Script/MoveComponent.cs
@@ -6,6 +6,13 @@ public class MoveComponent : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 30.0f;
+    private float _angleX = 0.0f;
+    private float _angleY = 0.0f;
+    private float _angleZ = 0.0f;
+
+    private float _xDistance = 0.0f;
+    private float _yDistance = 0.0f;
+    private float _zDistance = 0.0f;
 
     void Start()
     {
@@ -19,19 +26,29 @@ public class MoveComponent : MonoBehaviour
         // 1초 Update를 10번하는 컴퓨터 : +10 * forward -> 시간변화량 1/10   => 1
         // deltatime : 시간변화량
         // w
+        // Move
         if (Input.GetKey(KeyCode.W))
-            transform.position += Vector3.forward * Time.deltaTime * _speed;
+        {
+            _zDistance += Time.deltaTime * _speed;
+        }
         // s
         if (Input.GetKey(KeyCode.S))
-            transform.position += Vector3.back * Time.deltaTime * _speed;
+        {
+            _zDistance -= Time.deltaTime * _speed;
+        }
 
         // a
         if (Input.GetKey(KeyCode.A))
-            transform.position += Vector3.left * Time.deltaTime * _speed;
-
+        {
+            _xDistance += Time.deltaTime * _speed;
+        }
         // d
         if (Input.GetKey(KeyCode.D))
-            transform.position += Vector3.right * Time.deltaTime * _speed;
+        {
+            _xDistance -= Time.deltaTime * _speed;
+        }
+
+        transform.Translate(new Vector3(_xDistance, 0, _zDistance));
 
         // Scaling
         // z
@@ -40,5 +57,19 @@ public class MoveComponent : MonoBehaviour
         if (Input.GetKey(KeyCode.P))
             transform.localScale -= Vector3.one * Time.deltaTime;
 
+        // Rotation
+        _angleX += Time.deltaTime * _speed;
+        _angleY += Time.deltaTime * _speed;
+        _angleZ += Time.deltaTime * _speed;
+
+        // Y축
+        if (Input.GetKey(KeyCode.Z))
+            transform.rotation = Quaternion.Euler(new Vector3(0,_angleY,0));
+        // X축
+        if (Input.GetKey(KeyCode.X))
+            transform.rotation = Quaternion.Euler(new Vector3(_angleX, 0, 0));
+        // Z축
+        if (Input.GetKey(KeyCode.C))
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, _angleZ));
     }
 }
